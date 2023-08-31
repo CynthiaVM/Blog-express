3bdf414313feb8fb0c4f185e7faa9597eea0b947
CynthiaVM <80722354+CynthiaVM@users.noreply.github.com>
2023-08-30 21:16:29 -0300

    Create server.ts

---
+++ server.ts
@@ -0,0 +1,14 @@
+import express, { Express } from 'express';
+import noticiasRoutes from './modules/noticias/noticia.routes';
+import bodyParser from 'body-parser';
+
+const app: Express = express();
+
+//instancia global de express es app
+app.use(bodyParser.json());
+
+//app.use('/noticia', noticiasRoutes);
+
+app.listen(2000, () => {
+	console.log('Servidor funcionando OK en el PORT: 2000');
+});
